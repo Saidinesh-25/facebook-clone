84ed1cb4378cdb94847a4d9ef5b7703a85646fd3
Sai Dinesh <said@Sais-MacBook-Air.local>
2024-07-17 21:03:17 +0530

    header center portion

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,11 +1,21 @@
 import React from "react";
 import Image from "next/image";
+import {
+  HomeIcon,
+  PlayIcon,
+  ShoppingCartIcon,
+  FlagIcon,
+  UserGroupIcon,
+  BellIcon,
+} from "@heroicons/react/16/solid";
+import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
+import HeaderIcon from "./HeaderIcon";
 
 function Header() {
   return (
-    <div>
-      <h1>Header</h1>
-      <div>
+    <div className="sticky top-0 z-50 bg-white flex items-center p-2 lg:px-5 shadow-md">
+      {/* left */}
+      <div className="flex items-center">
         <Image
           src="https://links.papareact.com/5me"
           width={40}
@@ -13,6 +23,31 @@ function Header() {
           layout="fixed"
           alt="someimg"
         />
+        <div className="flex ml-2 items-center rounded-full bg-gray-100 p-2">
+          <MagnifyingGlassIcon className="h-6 text-gray-600" />
+          <input
+            className="hidden md:inline-flex ml-2 items-center bg-transparent outline-none placeholder-gray-500 flex-shrink"
+            type="text"
+            placeholder="Search Facebook"
+          />
+        </div>
+      </div>
+      {/* center */}
+      <div className="flex justify-center flex-grow">
+        <div className="flex space-x-6 md:space-x-2">
+          <HeaderIcon Icon={HomeIcon} />
+          <HeaderIcon Icon={FlagIcon} />
+          <HeaderIcon Icon={PlayIcon} />
+          <HeaderIcon Icon={ShoppingCartIcon} />
+          <HeaderIcon Icon={UserGroupIcon} />
+        </div>
+      </div>
+      {/* right */}
+      <div className="flex items-center sm:space-x-2 justify-end">
+        <p className="whitespace-nowrap font-semibold pr-3">Sai Dinesh</p>
+        {/* <ViewGridIcon className="icon" />
+        <ChatIcon className="icon" /> */}
+        <BellIcon className="icon" />
       </div>
     </div>
   );
